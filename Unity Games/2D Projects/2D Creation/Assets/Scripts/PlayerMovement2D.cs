56663f8b12362814d8cc0e248277c626b70e21c7
iseandree <iezell2@uccs.edu>
2025-11-21 23:28:49 -0700

    Updated the Player script
    Added response to an interact action, currently nothing can be collected but the basis is made. The player detects what is near them that can be collected (it currently detects the borders of the game scene) and when the interact button is pressed they will get a response via debug.log.

--- Unity Games/2D Projects/2D Creation/Assets/Scripts/PlayerMovement2D.cs
+++ Unity Games/2D Projects/2D Creation/Assets/Scripts/PlayerMovement2D.cs
@@ -3,28 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement2D : MonoBehaviour
 {
+    // Serialized fields
     [SerializeField] private float moveSpeed = 3.0f;
     [SerializeField] private float jumpForce = 6.0f;
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isNear;
     [SerializeField] private float jumpCooldown = 0.50f;
+    [SerializeField] private float circleCastRadius = 0.75f;
+    
+    // Private Variables
     private float lastJumpTime;
     private PlayerInput playerInput;
     private InputAction moveAction;
     private InputAction jumpAction;
+    private InputAction interactAction;
     private Rigidbody2D rb;
-    
     private LayerMask groundLayer;
+    private LayerMask borderLayer;
     
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
         groundLayer = LayerMask.GetMask("Ground");
+        borderLayer = LayerMask.GetMask("Border");
         moveAction = playerInput.actions.FindAction("Move");
         jumpAction = playerInput.actions.FindAction("Jump");
+        interactAction = playerInput.actions.FindAction("Interact");
         lastJumpTime = -jumpCooldown;
     }
 
@@ -37,14 +43,39 @@ public class PlayerMovement2D : MonoBehaviour
 
     private void FixedUpdate()
     {
-       isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.75f, groundLayer);
+        // Check if the player is grounded
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.75f, groundLayer);
         Debug.DrawRay(transform.position, Vector2.down, Color.black);
-        //rb.linearVelocityX = moveAction.ReadValue<Vector2>().x * moveSpeed;
+
+
+        // Check if the player is near a collectible object
+        isNear = Physics2D.CircleCast(transform.position, circleCastRadius, Vector2.zero, 1f, borderLayer);
+        
+        if (isNear && interactAction.WasPressedThisFrame())
+        {
+            Collect();
+        }
+
+        //rb.linearVelocityX = moveAction.ReadValue<Vector2>().x * moveSpeed;  Old way to move the player horizontally
+
         rb.AddForce(Vector2.right * moveAction.ReadValue<Vector2>().x * moveSpeed, ForceMode2D.Force);
         if (jumpAction.IsPressed() && isGrounded && Time.time >= lastJumpTime + jumpCooldown)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             lastJumpTime = Time.time;
         }
+
+
+    }
+
+    private void Collect()
+    {
+        
+        Debug.Log("Interact Pressed within range");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, circleCastRadius);
     }
 }
