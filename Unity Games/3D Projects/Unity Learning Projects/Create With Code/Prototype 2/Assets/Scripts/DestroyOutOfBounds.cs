a6cc7dea878d3c53cda99436055b3d32fd205635
iseandree <iezell2@uccs.edu>
2026-01-09 14:18:05 -0700

    Lesson 2.4 Completed
    Lesson 2.4 in the Junior Programming Unity Learn had us changing the spawn manager from being activated via a key press to a timer. We also dealt with collisions and generating a "Game Over" text when the animals get past the player.

--- Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -9,18 +9,20 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z > topBound)
+        // If an object goes past the player's view, remove that object
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
-        else if(transform.position.z < lowerBound)
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -6,21 +6,26 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject[] animalPrefabs;
     private float spawnRangeX = 15;
     private float spawnRangeZ = 20;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+
+    // Spawn a random animal from the animalPrefabs array in random locations
+    private void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
