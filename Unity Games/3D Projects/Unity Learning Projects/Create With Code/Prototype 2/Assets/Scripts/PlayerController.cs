f9477672c0dee0f61a94a3787daa62adb2813023
iseandree <iezell2@uccs.edu>
2026-01-09 10:10:13 -0700

    Lesson 2.1 completed
    Lesson 2.1 in the Junior Programming Unity Learn had us moving the player character horizontally and using if statements to keep them in bounds.

---
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Private Variables
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float speed = 10.0f;
+    private float xRange = 15.0f;
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep the player within the bounds
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+}
