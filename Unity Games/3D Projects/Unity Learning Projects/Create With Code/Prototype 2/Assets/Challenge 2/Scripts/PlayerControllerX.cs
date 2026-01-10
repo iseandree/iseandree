c8add87d9aad6b8f9a63ce843089d25d90f6fd3b
iseandree <iezell2@uccs.edu>
2026-01-09 17:56:10 -0700

    Completed Challenge 2
    Challenge 2 in the Junior Programmer unity learn was mostly bug fixes. I had to fix the spawning of the ball and the dog prefabs and I also had to prevent spamming of the dog spawning via player input.

---
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastSpawnTime;
+    [SerializeField] private float spawnDelay = 1.0f;
+
+    private void Start()
+    {
+        lastSpawnTime = Time.time;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastSpawnTime += Time.deltaTime;
+        // On spacebar press, send dog
+        if (lastSpawnTime >= spawnDelay)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastSpawnTime = 0;
+            }
+        }
+    }
+}
