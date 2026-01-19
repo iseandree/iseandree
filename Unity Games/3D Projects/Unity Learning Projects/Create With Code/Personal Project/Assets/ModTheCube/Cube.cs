cbcee12ae9b02899668fb8e89b92a4e711509fde
iseandree <iezell2@uccs.edu>
2026-01-18 18:11:28 -0700

    Mod The Cube Completed
    Was given a script and an object, the cube, and was told to modify it anyway that I see fit; I changed the transform, randomized the rotation speed of the cube, and lerped the color.

---
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Personal Project/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Color defaultColor = Color.white;
+    [SerializeField] private Color highlightColor = Color.blue;
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private float lerpSpeed = 1f;
+    private Material mat;
+    private float time;
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        rotationSpeed = Random.Range(0.0f, 40.0f);
+        mat = meshRenderer.material;
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0.0f, 0.0f);
+        transform.Translate(moveSpeed * Time.deltaTime, 0.0f, moveSpeed * Time.deltaTime);
+        time += Time.deltaTime * lerpSpeed;
+        float lerpValue = Mathf.PingPong(time, 1f);
+        mat.color = Color.Lerp(defaultColor, highlightColor, lerpValue);
+    }
+
+}
