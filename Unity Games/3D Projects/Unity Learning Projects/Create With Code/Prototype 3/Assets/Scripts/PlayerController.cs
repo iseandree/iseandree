0e3ad058d5c208583fb7aa82d960b798238c182c
iseandree <iezell2@uccs.edu>
2026-01-26 16:41:47 -0700

    Lesson 3.3 Completed
    In this lesson we were tasked with adding animations to the player character and fine tuning variables to improve game feel.

--- Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     // Private variables
     private Rigidbody playerRb;
+    private Animator playerAnim;
     [SerializeField] private float jumpForce = 10.0f;
     [SerializeField] private float gravityModifier;
     private bool isGrounded = true;
@@ -15,16 +16,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over.");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
