29d6dfd2ec8a86734a00f0f014be604d6f4dcb51
iseandree <iezell2@uccs.edu>
2026-02-20 00:08:47 -0700

    Started the Counting Prototype for Junior Programmer
     This unity learn section wants us to add to our portfolio and build out a prototype with a given game idea, a game where balls land into a box and are counted. I decided to go with a pachinko-like game.

---
+++ Unity Games/3D Projects/Unity Learning Projects/Create With Code/Personal Project/Assets/CongratScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+
+    [SerializeField] private List<string> TextToDisplay;
+
+    [SerializeField] private float RotatingSpeed;
+    [SerializeField] private float TimeToNextText;
+
+    [SerializeField] private int CurrentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+
+        RotatingSpeed = 1.0f;
+
+        TextToDisplay.Add("Congratulation");
+        TextToDisplay.Add("All Errors Fixed");
+
+        Text.text = TextToDisplay[CurrentText];
+
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeToNextText += Time.deltaTime;
+
+        if (TimeToNextText > RotatingSpeed)
+        {
+            TimeToNextText = 0.0f;
+
+            CurrentText++;
+            
+            if (CurrentText == TextToDisplay.Count)
+            {
+                CurrentText = 0;
+            }
+            Text.text = TextToDisplay[CurrentText];
+        }
+    }
+}
