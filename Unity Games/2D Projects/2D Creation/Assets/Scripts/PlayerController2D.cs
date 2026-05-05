83ab17e00d8b79fb3e8133e6b1122f0a48a9d4ad
iseandree <iezell2@uccs.edu>
2026-05-05 17:10:23 -0600

    Added animations
    I created a myriad of animations for the player and npcs and added them all to the project.

--- Unity Games/2D Projects/2D Creation/Assets/Scripts/PlayerController2D.cs
+++ Unity Games/2D Projects/2D Creation/Assets/Scripts/PlayerController2D.cs
@@ -12,6 +12,7 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField] private bool isNear;
     [SerializeField] private float jumpCooldown = 0.50f;
     [SerializeField] private float circleCastRadius = 0.75f;
+    [SerializeField] private float rayCastLength = 2f;
 
     // Private Variables
     private float lastJumpTime;
@@ -23,11 +24,20 @@ public class PlayerController2D : MonoBehaviour
     private LayerMask groundLayer;
     private LayerMask borderLayer;
     private GameObject interactable;
+    private bool isFacingLeft = false;
+    private bool isMoving = false;
+    private bool isMovingLeft;
+    private bool isMovingRight;
+    private Animator animator;
+
+    // Public Variables
     public List<GameObject> inventory;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
         groundLayer = LayerMask.GetMask("Ground");
@@ -36,6 +46,7 @@ public class PlayerController2D : MonoBehaviour
         jumpAction = playerInput.actions.FindAction("Jump");
         interactAction = playerInput.actions.FindAction("Interact");
         lastJumpTime = -jumpCooldown;
+
     }
 
     // Update is called once per frame
@@ -52,22 +63,43 @@ public class PlayerController2D : MonoBehaviour
     private void FixedUpdate()
     {
         // Check if the player is grounded
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.75f, groundLayer);
-        Debug.DrawRay(transform.position, Vector2.down, Color.black);
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, rayCastLength, groundLayer);
+        Debug.DrawRay(transform.position, Vector2.down * rayCastLength, Color.green);
+        Debug.Log(isGrounded);
 
+        // Get horizontal input
+        float moveInput = moveAction.ReadValue<Vector2>().x;
+        
+        // Apply Movement Force
+        rb.AddForce(Vector2.right * moveInput * moveSpeed, ForceMode2D.Force);
 
+        // Update 'isWalking' based on whether there is any input (regardless of direction)
+        bool inputPresent = Mathf.Abs(moveInput) > 0.1f;
+        animator.SetBool("isWalking", inputPresent);
 
+        // Handle flipping separately
+        if (moveInput < 0 && !isFacingLeft)
+        {
+            transform.Rotate(0.0f, 180f, 0.0f);
+            isFacingLeft = true;
+        }
+        else if (moveInput > 0 && isFacingLeft)
+        {
+            transform.Rotate(0.0f, 180f, 0.0f);
+            isFacingLeft = false;
+        }
 
-        //rb.linearVelocityX = moveAction.ReadValue<Vector2>().x * moveSpeed;  Old way to move the player horizontally
-
-        rb.AddForce(Vector2.right * moveAction.ReadValue<Vector2>().x * moveSpeed, ForceMode2D.Force);
+        // Prevent the player from spamming jump
         if (jumpAction.IsPressed() && isGrounded && Time.time >= lastJumpTime + jumpCooldown)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             lastJumpTime = Time.time;
+            animator.SetBool("isJumping", true);
         }
-
-
+        else if(isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 
     private bool DetectInteractable()
@@ -76,7 +108,7 @@ public class PlayerController2D : MonoBehaviour
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, circleCastRadius);
 
         // Check if there are any colliders within the range of the player. If not close the method
-        if(hits.Length == 0)
+        if (hits.Length == 0)
         {
             interactable = null;
             Debug.Log("No collisions detected");
@@ -89,15 +121,15 @@ public class PlayerController2D : MonoBehaviour
 
         /* Loop through all possible colliders within the range of the player and determine if:
          A) They have the required Tag and B) Which ideal interactable is closest to the player */
-        foreach(Collider2D hit in hits)
+        foreach (Collider2D hit in hits)
         {
-            if(!hit.CompareTag("Interactable"))
+            if (!hit.CompareTag("Interactable"))
             {
                 continue;
             }
 
             float distanceFromPlayer = Vector2.Distance(transform.position, hit.transform.position);
-            if(distanceFromPlayer < distanceFromMatching)
+            if (distanceFromPlayer < distanceFromMatching)
             {
                 // Store the ideal Collider as the confirmed matching collider and store its distance from the player
                 matchingInteractable = hit;
@@ -106,12 +138,12 @@ public class PlayerController2D : MonoBehaviour
         }
 
         // Check if the ideal Collider has been assigned a match, if not close the method
-        if(matchingInteractable == null)
+        if (matchingInteractable == null)
         {
             interactable = null;
             return false;
         }
-        
+
         // Store the game object attached to the ideal Collider to the confirmed interactable and return true for this method. 
         interactable = matchingInteractable.gameObject;
         return true;
