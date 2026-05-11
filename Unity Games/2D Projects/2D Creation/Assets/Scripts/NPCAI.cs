802eeb9b723a41e812fa5a44c31551d42c81b87f
iseandree <iezell2@uccs.edu>
2026-05-11 02:32:50 -0600

    Setting up the AI for the NPCs
    Started the process of adding AI to the npcs that will wander the background of the game. So far they can walk back and forth and wait, un-animated.

---
+++ Unity Games/2D Projects/2D Creation/Assets/Scripts/NPCAI.cs
@@ -0,0 +1,77 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+
+
+public class NPCAI : MonoBehaviour
+{
+    // Serialize Field
+    [SerializeField] private float leftPatrolX;
+    [SerializeField] private float rightPatrolX;
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private float minPauseTime;
+    [SerializeField] private float maxPauseTime;
+    [SerializeField] private float minWalkTime;
+    [SerializeField] private float maxWalkTime;
+
+    // Private Variables
+    private Rigidbody2D rb;
+    private int facingDirection = -1;
+    private float randomTime;
+    private float timer;
+    private bool isWalking = true;
+    private bool isFlipping = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        randomTime = Random.Range(minWalkTime, maxWalkTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= randomTime)
+        {
+            StateChange();
+        }
+        if (!isFlipping && (transform.position.x < leftPatrolX || transform.position.x > rightPatrolX))
+        {
+            StartCoroutine(Flip());
+        }
+
+        if (isWalking)
+        {
+            rb.linearVelocity = Vector2.right * facingDirection * moveSpeed;
+        }
+
+    }
+
+    private void StateChange()
+    {
+        isWalking = !isWalking;
+        if (isWalking)
+        {
+            randomTime = Random.Range(minWalkTime, maxWalkTime);
+        }
+        else
+        {
+            randomTime = Random.Range(minPauseTime, maxPauseTime);
+        }
+        timer = 0;
+    }
+
+    IEnumerator Flip()
+    {
+        isFlipping = true;
+        transform.Rotate(0, 180, 0);
+        facingDirection *= -1;
+        yield return new WaitForSeconds(0.5f);
+        isFlipping = false;
+    }
+}
